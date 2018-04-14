8c2d6b69737cd4201f5ea15588a53db1cf3508cd
samuele.malavasi <samuele.malavasi@gmail.com>
2018-04-14 20:55:31 +0200

    added env variables

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  mode: 'Development',
+  api_endpoint: 'https://8kn6qtdbae.execute-api.eu-central-1.amazonaws.com/dev'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  mode: 'Production',
+  api_endpoint: 'https://oa7scwswmd.execute-api.eu-central-1.amazonaws.com/prod'
+}
