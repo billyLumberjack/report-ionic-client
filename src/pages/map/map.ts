1c326d14759d3c6ad241864d44cf8d245d387266
smalavasi <samuele.malavasi@amadeus.com>
2020-03-20 01:36:42 +0100

    small code refactoring

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -27,52 +27,44 @@ export class MapPage {
   ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
-
     this.map = this.mapProvider.getMap("map1");
-
     this.loadmap();
   }
 
   loadmap(){
 
     console.log("loadmap triggered with " + this.shared.data.length + " reports");
-
     let markerArray = [];
 
     this.shared.data.forEach((report_obj, index) => {
-
       if(report_obj.geometry.type){
+        let popup_string = '<table>'+
+          '<tr>'+
+          '<th colspan="2"><a onclick="document.dispatchEvent(new CustomEvent(\'build\',{detail: '+index+'}));" href="javascript:void(0);"><b>'+
+          report_obj.TripName+
+          '</b></a><th>'+
+          '</tr>'+
+          '<tr>'+
+          '<td>'+this.translate.instant("DETAILS.ElevationGain")+'</td>'+
+          '<td class="popup-value">'+report_obj.ElevationGain +'</td>'+
+          '</tr>'+
+          '<tr>'+
+          '<td>'+this.translate.instant("DETAILS.Grade")+'</td>'+
+          '<td class="popup-value">'+report_obj.Grade +'</td>'+
+          '</tr>'+
+          '<tr>'+
+          '<td>'+this.translate.instant("DETAILS.TripRate")+'</td>'+
+          '<td class="popup-value">'+report_obj.TripRate +'</td>'+
+          '</tr></table>';
 
-      let popup_string = '<table>'+
-        '<tr>'+
-        '<th colspan="2"><a onclick="document.dispatchEvent(new CustomEvent(\'build\',{detail: '+index+'}));" href="javascript:void(0);"><b>'+
-        report_obj.TripName+
-        '</b></a><th>'+
-        '</tr>'+
-        '<tr>'+
-        '<td>'+this.translate.instant("DETAILS.ElevationGain")+'</td>'+
-        '<td class="popup-value">'+report_obj.ElevationGain +'</td>'+
-        '</tr>'+
-        '<tr>'+
-        '<td>'+this.translate.instant("DETAILS.Grade")+'</td>'+
-        '<td class="popup-value">'+report_obj.Grade +'</td>'+
-        '</tr>'+
-        '<tr>'+
-        '<td>'+this.translate.instant("DETAILS.TripRate")+'</td>'+
-        '<td class="popup-value">'+report_obj.TripRate +'</td>'+
-        '</tr></table>';
-
-      markerArray.push(
-        new leaflet.Marker(report_obj.geometry.coordinates)
-        .bindPopup(popup_string)
-      );
+        markerArray.push(
+          new leaflet.Marker(report_obj.geometry.coordinates)
+            .bindPopup(popup_string)
+        );
       }
-
     });
 
     document.addEventListener('build', (event) => {
-        //console.log(JSON.stringify(event.detail,null,2));
 
         this.navCtrl.push(ReportDetailsPage, {
           report: this.shared.data[event["detail"]]
@@ -84,10 +76,6 @@ export class MapPage {
     markerGroup.addTo(this.map);
     this.map.fitBounds(markerGroup.getBounds());
 
-  }
-
-  zoomIn(){
-    this.map.setZoom(this.map.getZoom() + 1);
   }
 
 }
