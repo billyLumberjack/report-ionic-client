c5ed2f8af58565cb75789db15f71f276c4d1c734
samuele.malavasi <samuele.malavasi@gmail.com>
2020-04-07 00:50:43 +0200

    minor code refactoring

--- create_signed_apk.sh
+++ create_signed_apk.sh
@@ -6,19 +6,19 @@ jarsigner \
     -sigalg SHA1withRSA \
     -digestalg SHA1 \
     -keystore play-store-private-key.keystore \
-    /Users/samuele/Desktop/MY_PROJECT/manna/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk \
+    ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk \
     play-store-private-key
 
 zipalign \
     -v 4 \
-    /Users/samuele/Desktop/MY_PROJECT/manna/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk \
-    /Users/samuele/Desktop/MY_PROJECT/manna/platforms/android/app/build/outputs/apk/release/app-release-signed.apk
+    ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk \
+    ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
 
-rm /Users/samuele/Desktop/MY_PROJECT/manna/platforms/android/app/build/outputs/apk/release/app-release-signed.apk
+rm ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
 
 /Users/samuele/Library/Android/sdk/build-tools/27.0.3/zipalign \
     -v 4 \
-    /Users/samuele/Desktop/MY_PROJECT/manna/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk \
-    /Users/samuele/Desktop/MY_PROJECT/manna/platforms/android/app/build/outputs/apk/release/app-release-signed.apk
+    ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk \
+    ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
     
-open /Users/samuele/Desktop/MY_PROJECT/manna/platforms/android/app/build/outputs/apk/release/
+open ./platforms/android/app/build/outputs/apk/release/
