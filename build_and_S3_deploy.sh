d5c5747f3664d262e1b90b4a9a84a690f8aa14f6
samuele.malavasi <samuele.malavasi@gmail.com>
2020-04-30 00:08:15 +0200

    added step to clean S3 bucket

--- build_and_S3_deploy.sh
+++ build_and_S3_deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+aws s3 rm s3://web-skiapp/
 ionic cordova build browser --prod
 aws s3 cp /Users/samuele/Desktop/MY_PROJECT/ionic_skiapp/platforms/browser/www s3://web-skiapp/ --acl public-read --recursive
