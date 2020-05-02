a22b50331b97109540e565f7ea745535eb674c0d
samuele.malavasi <samuele.malavasi@gmail.com>
2020-05-02 21:39:40 +0200

    changed flow for new version assignment

--- create_signed_apk_with_version.sh
+++ create_signed_apk_with_version.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-. get_and_increase_tag.sh
+#!/bin/bash
+
+#get highest tag number
+VERSION=`git describe --abbrev=0 --tags`
+
+#replace . with space so can split into an array
+VERSION_BITS=(${VERSION//./ })
+
+#get number parts and increase last one by 1
+VNUM1=${VERSION_BITS[0]}
+VNUM2=${VERSION_BITS[1]}
+VNUM3=${VERSION_BITS[2]}
+VNUM3=$((VNUM3+1))
+
+#create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 
 to_find_in_config="version=\"[0-9 .]*\""
 to_replace_in_config="version=\"$NEW_TAG\""
@@ -9,6 +24,9 @@ sed -i .backup "s/$to_find_in_config/$to_replace_in_config/" ./config.xml
 to_find_in_json="\"version\": \"[0-9 .]*\""
 to_replace_in_json="\"version\": \"$NEW_TAG\""
 sed -i .backup "s/$to_find_in_json/$to_replace_in_json/" ./package.json
+
+git add config.xml package.json
+git commit -m "Commit for version $NEW_TAG"
 
 ionic cordova build android --prod --release
 #keytool -genkey -v -keystore play-store-private-key.keystore -alias play-store-private-key -keyalg RSA -keysize 2048 -validity 10000
@@ -32,4 +50,17 @@ rm ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
     ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk \
     ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
     
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+#only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+    git tag $NEW_TAG
+    echo "Tagged with $NEW_TAG"
+    git push --tags
+else
+    echo "Already a tag on this commit"
+fi
+
 open ./platforms/android/app/build/outputs/apk/release/
